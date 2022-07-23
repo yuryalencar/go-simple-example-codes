3f7d3eb72ed035c7c1d97d5ed6ad0cd8566da54a
Yury Alencar <yury.alencar@condoconta.com.br>
2022-07-23 09:34:16 -0300

    feat(file): added method to save deck in file

--- cards/deck.go
+++ cards/deck.go
@@ -1,31 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string
 
 func newDeck() deck {
-    cards := deck{}
+	cards := deck{}
 
-    cardSuits := []string {"Spades", "Diamonds", "Hearts", "Clubs"}
-    cardValues := []string {"Ace", "Two", "Three", "Four"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-    for _, suit := range cardSuits {
-        for _, value := range cardValues {
-            cards = append(cards, value+" of "+suit)
-        }
-    }
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
 
-    return cards
+	return cards
 }
 
 func (d deck) print() {
-    fmt.Println("CARDS LIST")
-    for _, card := range d {
-        fmt.Println(card)
-    }
+	fmt.Println("CARDS LIST")
+	for _, card := range d {
+		fmt.Println(card)
+	}
 }
 
 func deal(d deck, handSize int) (deck, deck) {
-    return d[:handSize], d[handSize:]
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
