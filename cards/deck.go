35715edceee8a1dec9a12017947ca13c5c868fda
Yury Alencar <yury.alencar@condoconta.com.br>
2022-07-23 09:59:55 -0300

    feat(file): added method to read deck from disk hard file

--- cards/deck.go
+++ cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -24,7 +25,6 @@ func newDeck() deck {
 }
 
 func (d deck) print() {
-	fmt.Println("CARDS LIST")
 	for _, card := range d {
 		fmt.Println(card)
 	}
@@ -40,4 +40,16 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,9 @@
 package main
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards.txt")
+	cards := newDeckFromFile("my_cards.txt")
+	cards.print()
+
+	// cards := newDeck()
+	// cards.saveToFile("my_cards.txt")
 }
