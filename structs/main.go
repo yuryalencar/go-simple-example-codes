955b1ea0a019ab53e4736571587a5a09995bdb60
Yury Alencar <yury.alencar@condoconta.com.br>
2022-07-31 11:55:18 -0300

    feat(structs): added new example to initialize a sctruct

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	yury := person{firstName: "Yury", lastName: "Alencar"}
+	// yury := person{firstName: "Yury", lastName: "Alencar"}
 
-	fmt.Println("Struct:", yury)
-	fmt.Println("First name:", yury.firstName)
-	fmt.Println("Last name:", yury.lastName)
+	// fmt.Println("Struct:", yury)
+	// fmt.Println("First name:", yury.firstName)
+	// fmt.Println("Last name:", yury.lastName)
+
+	ben := person{
+		firstName: "Ben",
+		lastName:  "Junior",
+		contact: contactInfo{
+			email:   "benjunior@gmail.com",
+			zipCode: 88106444,
+		},
+	}
+
+	fmt.Printf("%+v", ben)
 }
